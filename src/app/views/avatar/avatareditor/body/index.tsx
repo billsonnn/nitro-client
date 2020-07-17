6d8de7c853ef1a278fcbe2a63a6f4ec1fdb2c13d
Beny <beny@krews.org>
2020-07-17 03:49:11 +0200

    Fix gender word spacing

--- src/app/views/avatar/avatareditor/body/index.tsx
+++ src/app/views/avatar/avatareditor/body/index.tsx
@@ -26,7 +26,7 @@ export function AvatarEditorBodyView(props: AvatarEditorBodyViewProps): JSX.Elem
         <div className="view-container">
             <div className="view-categories">
                 {genders.map((value, index) => {
-                return <button type="button" className={ "btn btn-destiny" + (props.gender === value.gender ? ' active' : '') } key={index} onClick={ event => props.setGenderHandler(value.gender) }>{ (value.icon && <i className={ "icon " + value.icon + (props.gender === value.gender ? ' selected' : '')} />) } { value.name }</button>
+                return <button type="button" className={ "btn btn-destiny" + (props.gender === value.gender ? ' active' : '') } key={index} onClick={ event => props.setGenderHandler(value.gender) }>{ (value.icon && <i className={ "icon " + value.icon + (props.gender === value.gender ? ' selected' : '')} />) }&nbsp;&nbsp;{ value.name }</button>
                 })}
             </div>
             <AvatarEditorSet setName={ 'hd' } gender={ props.gender } />
