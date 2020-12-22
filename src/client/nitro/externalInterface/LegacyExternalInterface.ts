4a0d60e2c18deb4ef7a6c14090c983a4106c05fd
Watson HRW <watson@hrw.pw>
2020-12-22 19:24:22 +0200

    Use postMessage to signal legacy events to top

--- src/client/nitro/externalInterface/LegacyExternalInterface.ts
+++ src/client/nitro/externalInterface/LegacyExternalInterface.ts
@@ -7,6 +7,14 @@ export class LegacyExternalInterface implements ILegacyExternalInterface
 		...params: Parameters<typeof window.FlashExternalInterface[K]>
 	): ReturnType<typeof window.FlashExternalInterface[K]> | undefined
 	{
+		if(window.top !== window)
+		{
+			window.top.postMessage('Nitro_LegacyExternalInterface' + JSON.stringify({
+				method,
+				params
+			}), '*');
+		}
+
 		if(!('FlashExternalInterface' in window)) return undefined;
 
 		const fn = window.FlashExternalInterface[method] as Function;
@@ -19,6 +27,14 @@ export class LegacyExternalInterface implements ILegacyExternalInterface
 		...params: Parameters<typeof window.FlashExternalGameInterface[K]>
 	): ReturnType<typeof window.FlashExternalGameInterface[K]> | undefined
 	{
+		if(window.top !== window)
+		{
+			window.top.postMessage('Nitro_LegacyExternalGameInterface' + JSON.stringify({
+				method,
+				params
+			}), '*');
+		}
+
 		if(!('FlashExternalGameInterface' in window)) return undefined;
 
 		const fn = window.FlashExternalGameInterface[method] as Function;
