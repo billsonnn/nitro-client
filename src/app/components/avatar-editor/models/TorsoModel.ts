c61e678b99d620a42d5442eaab240e8e0d47b492
Bill <billsonnn@live.com>
2021-01-12 22:10:54 -0500

    Change categories order

--- src/app/components/avatar-editor/models/TorsoModel.ts
+++ src/app/components/avatar-editor/models/TorsoModel.ts
@@ -8,9 +8,9 @@ export class TorsoModel extends CategoryBaseModel
     {
         super.init();
 
-        this._Str_3130(FigureData.CC);
         this._Str_3130(FigureData.CH);
         this._Str_3130(FigureData.CHEST_ACCESSORIES);
+        this._Str_3130(FigureData.CC);
         this._Str_3130(FigureData.CHEST_PRINTS);
 
         this._Str_2367 = true;
