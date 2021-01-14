c24c7b1dc66040f8b353593788283ccf95ad94fb
Bill <billsonnn@live.com>
2021-01-13 23:07:09 -0500

    Fix composer

--- src/client/nitro/communication/messages/outgoing/navigator/NavigatorCategoryListModeComposer.ts
+++ src/client/nitro/communication/messages/outgoing/navigator/NavigatorCategoryListModeComposer.ts
@@ -1,15 +1,15 @@
 import { IMessageComposer } from '../../../../../core/communication/messages/IMessageComposer';
 
-export class NavigatorCategoryListModeComposer implements IMessageComposer
+export class NavigatorCategoryListModeComposer implements IMessageComposer<ConstructorParameters<typeof NavigatorCategoryListModeComposer>>
 {
-    private _data: any[];
+    private _data: ConstructorParameters<typeof NavigatorCategoryListModeComposer>;
 
     constructor(category: string, listmode: number)
     {
         this._data = [category, listmode];
     }
 
-    public getMessageArray(): any[]
+    public getMessageArray()
     {
         return this._data;
     }
