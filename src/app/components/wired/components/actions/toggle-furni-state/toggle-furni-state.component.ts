6f93130f0978803b0ab2a559ee9887c99ae0fa8f
Remco <habboilany@gmail.com>
2021-01-15 23:07:50 +0100

    Refactored WiredAction

--- src/app/components/wired/components/actions/toggle-furni-state/toggle-furni-state.component.ts
+++ src/app/components/wired/components/actions/toggle-furni-state/toggle-furni-state.component.ts
@@ -1,75 +1,22 @@
-import { ActionDefinition } from './../../../../../../client/nitro/communication/messages/incoming/roomevents/ActionDefinition';
-import { WiredFurniture } from './../../../WiredFurniture';
 import { Component } from '@angular/core';
 import { WiredAction } from './../WiredAction';
-import { Triggerable } from '../../../../../../client/nitro/communication/messages/incoming/roomevents/Triggerable';
 import { WiredActionType } from '../WiredActionType';
 import { WiredMainComponent } from '../../main/main.component';
-import { Nitro } from 'src/client/nitro/Nitro';
-import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
     templateUrl: './toggle-furni-state.template.html'
 })
-export class ToggleFurniStateComponent extends WiredAction 
+export class ToggleFurniStateComponent extends WiredAction
 {
     public static CODE: number = WiredActionType.TOGGLE_FURNI_STATE;
 
-    private static MINIMUM_VALUE: number = 1;
-    private static MAXIMUM_VALUE: number = 20;
-    private static STEPPER_VALUE: number = 1;
-
-    public get code(): number 
+    public get code(): number
     {
         return ToggleFurniStateComponent.CODE;
     }
 
-    public onEditStart(trigger: Triggerable): void 
-    {
-        const actionDefinition = trigger as any as ActionDefinition;
-        this.delay = actionDefinition.delayInPulses;
-        this.updateLocaleParameter();
-    }
-
-    public onSliderChange(): void 
-    {
-        this.updateLocaleParameter();
-    }
-
-    public decrease(): void 
-    {
-        this.delay -= 1;
-
-        if(this.delay < ToggleFurniStateComponent.MINIMUM_VALUE) this.delay = ToggleFurniStateComponent.MINIMUM_VALUE;
-    }
-
-    public increase(): void 
-    {
-        this.delay += 1;
-
-        if(this.delay > ToggleFurniStateComponent.MAXIMUM_VALUE) this.delay = ToggleFurniStateComponent.MAXIMUM_VALUE;
-    }
-
-    protected updateLocaleParameter(): void 
-    {
-        Nitro.instance.localization.registerParameter('wiredfurni.params.delay', 'seconds', WiredFurniture.getLocaleName(this.delay));
-
-        this.updateCount++;
-    }
-
-    public get requiresFurni(): number 
+    public get requiresFurni(): number
     {
         return WiredMainComponent._Str_5430;
-    }
-
-    public get sliderOptions(): Options 
-    {
-        return {
-            floor: ToggleFurniStateComponent.MINIMUM_VALUE,
-            ceil: ToggleFurniStateComponent.MAXIMUM_VALUE,
-            step: ToggleFurniStateComponent.STEPPER_VALUE,
-            hidePointerLabels: true,
-            hideLimitLabels: true,
-        };
     }
 }
