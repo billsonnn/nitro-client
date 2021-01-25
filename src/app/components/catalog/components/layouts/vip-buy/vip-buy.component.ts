ea632cca3648473dc6dcbaae4a5adc2ec33ac633
Layne <laynebalsters@gmail.com>
2021-01-25 14:06:04 -0500

    new styles

--- src/app/components/catalog/components/layouts/vip-buy/vip-buy.component.ts
+++ src/app/components/catalog/components/layouts/vip-buy/vip-buy.component.ts
@@ -1,8 +1,8 @@
 import { Component, NgZone } from '@angular/core';
-import { CatalogLayout } from '../../../CatalogLayout';
-import { Nitro } from '../../../../../../client/nitro/Nitro';
-import { CatalogService } from '../../../services/catalog.service';
 import { CatalogClubOfferData } from '../../../../../../client/nitro/communication/messages/parser/catalog/utils/CatalogClubOfferData';
+import { Nitro } from '../../../../../../client/nitro/Nitro';
+import { CatalogLayout } from '../../../CatalogLayout';
+import { CatalogService } from '../../../services/catalog.service';
 
 
 @Component({
@@ -18,6 +18,7 @@ export class CatalogLayoutVipBuyComponent extends CatalogLayout
     {
         super(_catalogService, _ngZone);
         _catalogService.registerVipBuyTemplate(this);
+        
         _catalogService.requestOffers(6);
     }
 
@@ -25,16 +26,17 @@ export class CatalogLayoutVipBuyComponent extends CatalogLayout
     {
         this._ngZone.run(() =>
         {
-            this.vipOffers = offers;
+            this.vipOffers = offers.reverse();
         });
 
     }
 
-
     public getPurseContent(): string
     {
         const purse = this._catalogService.purse;
+
         const clubDays = purse.clubDays;
+
         const clubPeriods = purse.clubPeriods;
 
         const value = (clubPeriods * 31) + clubDays;
@@ -45,5 +47,19 @@ export class CatalogLayoutVipBuyComponent extends CatalogLayout
     public buyVip(offer: CatalogClubOfferData): void
     {
         this._catalogService.component && this._catalogService.component.confirmVipSubscription(offer);
+    }
+
+    public getCurrencyUrl(type: number): string
+    {
+        const url = Nitro.instance.getConfiguration<string>('currency.asset.icon.url');
+
+        return url.replace('%type%', type.toString());
+    }
+
+    public getOfferText(code: string)
+    { 
+        const str = Nitro.instance.getLocalizationWithParameter('friendlytime.months.short', 'amount', code.replace(/[^\d.]/g, ''));
+        
+        return str;
     }
 }
---
+++ src/app/components/catalog/purse/purse.ts
@@ -0,0 +1,15 @@
+import { IPurse } from './ipurse';
+
+export class Purse implements IPurse
+{
+    Str_14389: boolean;
+    clubDays: number;
+    clubPeriods: number;
+    credits: number;
+    isVip: boolean;
+    pastClubDays: number;
+    minutesUntilExpiration: number;
+
+
+
+}
