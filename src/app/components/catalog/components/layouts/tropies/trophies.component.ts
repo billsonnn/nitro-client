71a62ffc7b285e401cf8dab7321de29d891a16c9
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2021-01-30 18:38:02 +0100

    lots of debuggers

--- src/app/components/catalog/components/layouts/tropies/trophies.component.ts
+++ src/app/components/catalog/components/layouts/tropies/trophies.component.ts
@@ -3,12 +3,14 @@ import { CatalogClubOfferData } from '../../../../../../client/nitro/communicati
 import { Nitro } from '../../../../../../client/nitro/Nitro';
 import { CatalogLayout } from '../../../CatalogLayout';
 import { CatalogService } from '../../../services/catalog.service';
+import {Vector3d} from "../../../../../../client/room/utils/Vector3d";
+import {IGetImageListener} from "../../../../../../client/nitro/room/IGetImageListener";
 
 
 @Component({
     templateUrl: './trophies.template.html'
 })
-export class CatalogLayoutTrophiesComponent extends CatalogLayout implements OnInit
+export class CatalogLayoutTrophiesComponent extends CatalogLayout implements OnInit, IGetImageListener
 {
     public static CODE: string = 'trophies';
     public vipOffers: CatalogClubOfferData[] = [];
@@ -19,6 +21,22 @@ export class CatalogLayoutTrophiesComponent extends CatalogLayout implements OnI
         protected _ngZone: NgZone)
     {
         super(_catalogService, _ngZone);
+    }
+
+    imageReady(id: number, texture: PIXI.Texture, image?: HTMLImageElement): void {
+        debugger;
+        if((id === -1) || !image) return;
+
+        const url = image.src;
+
+        //if((this._iconUrl && this._iconUrl.length) && (url === this._iconUrl)) return;
+        debugger;
+//        this.setIconUrl(image.src);
+    //    throw new Error('Method not implemented.');
+    }
+    imageFailed(id: number): void {
+        debugger;
+      //  throw new Error('Method not implemented.');
     }
 
     ngOnInit(): void
@@ -38,6 +56,15 @@ export class CatalogLayoutTrophiesComponent extends CatalogLayout implements OnI
             {
                 this._trophyOffers.set(local4, new Map());
             }
+            // (typeId: number, direction: IVector3D, scale: number, listener: IGetImageListener, bgColor: number = 0, extras: string = null, state: number = -1, frameCount: number = -1, objectData: IObjectData = null): ImageResult
+
+
+            Nitro.instance.roomEngine.getFurnitureFloorImage(
+                item.products[0].furniClassId,
+                new Vector3d(2,0,0),
+                64,
+                this,
+                0, item.products[0].extraParam);
         });
 
         debugger;
