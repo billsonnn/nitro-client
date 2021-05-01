6e59ba5ca3c78e459f6c48d2edd6adaf761a5e79
skeletor <torresefrain10@gmail.com>
2021-05-01 17:45:11 +0000

    Feature/friendlist search

--- src/app/components/friendlist/components/friends-viewer/friends-viewer.component.ts
+++ src/app/components/friendlist/components/friends-viewer/friends-viewer.component.ts
@@ -14,6 +14,7 @@ export class FriendListFriendsViewerComponent
     private _requestsShowing: boolean = false;
     private _friendsShowing: boolean = true;
     private _onlineOnly: boolean = true;
+    private _searchShowing: boolean = false;
 
     constructor(
         private _friendListService: FriendListService,
@@ -30,6 +31,7 @@ export class FriendListFriendsViewerComponent
     public showFriends(): void
     {
         this._requestsShowing   = false;
+        this._searchShowing     = false;
         this._friendsShowing    = true;
         this._onlineOnly        = false;
     }
@@ -39,6 +41,15 @@ export class FriendListFriendsViewerComponent
         this._requestsShowing   = true;
         this._friendsShowing    = false;
         this._onlineOnly        = false;
+        this._searchShowing     = false;
+    }
+
+    public showSearch(): void
+    {
+        this._searchShowing = true;
+        this._friendsShowing = false;
+        this._requestsShowing = false;
+        this._onlineOnly = false;
     }
 
     public get requestsShowing(): boolean
@@ -54,6 +65,11 @@ export class FriendListFriendsViewerComponent
     public get onlineOnly(): boolean
     {
         return this._onlineOnly;
+    }
+
+    public get searchShowing(): boolean
+    {
+        return this._searchShowing;
     }
 
     public get totalFriendRequests(): number
