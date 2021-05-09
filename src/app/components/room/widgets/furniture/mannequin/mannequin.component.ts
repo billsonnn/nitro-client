e7f76a9702e9f5d00ff14c43dc22e6f4aca5ea8e
Dank074 <torresefrain10@gmail.com>
2021-05-09 03:00:17 -0500

    update figurecontainer variables

--- src/app/components/room/widgets/furniture/mannequin/mannequin.component.ts
+++ src/app/components/room/widgets/furniture/mannequin/mannequin.component.ts
@@ -125,7 +125,7 @@ export class MannequinWidget extends ConversionTrackingWidget
             case MannequinWidget.WrongGender:{
                 avatarFigureContainer = avatarRenderManager.createFigureContainer(this._figure);
                 this.removeSectionsFromAvatar(avatarFigureContainer);
-                this._renderedFigure = avatarFigureContainer._Str_1008();
+                this._renderedFigure = avatarFigureContainer.getFigureString();
             }
                 break;
             case MannequinWidget.UseMannequin: {
@@ -135,7 +135,7 @@ export class MannequinWidget extends ConversionTrackingWidget
             case MannequinWidget.EditorView: {
                 avatarFigureContainer = avatarRenderManager.createFigureContainer(this._figure);
                 this.removeSectionsFromAvatar(avatarFigureContainer);
-                this._renderedFigure = avatarFigureContainer._Str_1008();
+                this._renderedFigure = avatarFigureContainer.getFigureString();
 
 
             }
@@ -158,17 +158,17 @@ export class MannequinWidget extends ConversionTrackingWidget
     private removeSectionsFromAvatar(avatar:IAvatarFigureContainer): void
     {
 
-        for(const item of avatar._Str_1016())
+        for(const item of avatar.getPartTypeIds())
         {
 
             if(MannequinWidget.parts.indexOf(item) == -1)
             {
 
-                avatar._Str_923(item);
+                avatar.removePart(item);
             }
         }
 
-        avatar._Str_830(<string>MannequinWidget._Str_10597[0], <number>MannequinWidget._Str_10597[1], <number[]>MannequinWidget._Str_10597[2]);
+        avatar.updatePart(<string>MannequinWidget._Str_10597[0], <number>MannequinWidget._Str_10597[1], <number[]>MannequinWidget._Str_10597[2]);
     }
 
 
@@ -179,15 +179,15 @@ export class MannequinWidget extends ConversionTrackingWidget
 
         for(const part of MannequinWidget.parts)
         {
-            ownContainer._Str_923(part);
+            ownContainer.removePart(part);
         }
 
-        for(const part of addedContainer._Str_1016())
+        for(const part of addedContainer.getPartTypeIds())
         {
-            ownContainer._Str_830(part, addedContainer.getPartSetId(part), addedContainer._Str_815(part));
+            ownContainer.updatePart(part, addedContainer.getPartSetId(part), addedContainer.getPartColorIds(part));
         }
 
-        return ownContainer._Str_1008();
+        return ownContainer.getFigureString();
 
     }
 
